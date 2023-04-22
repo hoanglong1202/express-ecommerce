d8a91c392d649a6a9352763957200c6377ff13f6
Võ Đình Hoàng Long <cyberpunk2076@gmail.com>
2023-04-22 23:06:48 +0700

    add auth util

---
+++ src/utils/auth.utils.js
@@ -0,0 +1,20 @@
+"use strict";
+const jwt = require("jsonwebtoken");
+
+const createTokenPair = (payload, privateKey) => {
+  const accessToken = jwt.sign(payload, privateKey, {
+    algorithm: "RS256",
+    expiresIn: "2 days",
+  });
+
+  const refreshToken = jwt.sign(payload, privateKey, {
+    algorithm: "RS256",
+    expiresIn: "2 days",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+module.exports = {
+  createTokenPair,
+};
